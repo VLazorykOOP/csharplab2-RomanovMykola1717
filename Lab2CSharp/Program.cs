e8a4d2f62ed5baef9aa0c0e04da812cc2f303508
RomanovMykola1717 <162117283+RomanovMykola1717@users.noreply.github.com>
2024-06-07 07:24:08 +0300

    Add files via upload

--- Lab2CSharp/Program.cs
+++ Lab2CSharp/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace Lab2CSharp
+﻿using System;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        // Генеруємо випадковий масив
+        Random rand = new Random();
+        int arraySize = 20; // Розмір масиву
+        int[] array = new int[arraySize];
+        for (int i = 0; i < arraySize; i++)
         {
-            Console.WriteLine("Lab 2 CSharp");
+            array[i] = rand.Next(1, 101); // Випадкові числа від 1 до 100
         }
+
+        // Виводимо масив
+        Console.WriteLine("Згенерований масив:");
+        foreach (int num in array)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+
+        // Знаходимо мінімальний елемент
+        int minElement = array[0];
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] < minElement)
+            {
+                minElement = array[i];
+            }
+        }
+
+        // Знаходимо номер останнього мінімального елемента
+        int lastIndex = -1;
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            if (array[i] == minElement)
+            {
+                lastIndex = i;
+                break;
+            }
+        }
+
+        // Вивід результату
+        Console.WriteLine($"Мінімальний елемент: {minElement}");
+        Console.WriteLine($"Номер останнього мінімального елемента: {lastIndex}");
     }
 }
